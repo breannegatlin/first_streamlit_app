c3038b3077b96de888f84a30be80f33d3de3cb12
breannegatlin <137901358+breannegatlin@users.noreply.github.com>
2023-06-27 15:24:12 -0400

    Update streamlit_app.py
    move requests. add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -23,3 +22,11 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+#new section
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
+
