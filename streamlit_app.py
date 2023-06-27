f50f531cb815afb4aed40bff12617d9ed302dfba
breannegatlin <137901358+breannegatlin@users.noreply.github.com>
2023-06-27 15:26:52 -0400

    Update streamlit_app.py
    change web formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# make it pretty
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 
